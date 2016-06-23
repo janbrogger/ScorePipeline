fc0b8db8dc8e2df214a9473a9e2d84e852ad35c9
janbrogger <jan.brogger@holbergeeg.com>
2016-06-23 16:53:47 +0200

    Test import of one EEG using Nicolet Matlab SDK

---
+++ testimport.m
@@ -0,0 +1,18 @@
+
+%Get One Segment
+iSegment = 1;        					%One based so first segment of data
+vChannels = [];  					%Get Channels 2 and 5
+
+DataOneSegment = NicGetFullData(NicSt, iSegment, vChannels);
+
+%Get Time Chunk
+StartTime = NicSt.vSegmentStartTime(1);                %Get the starttime of the file
+
+SecondsIntoFile = 65;
+[ierr,ReadStartTime] = calllib('mat1','RecSecToDate',SecondsIntoFile, zeros(1));
+
+SecondsToRead = 8;
+data = NicGetData(NicSt, ReadStartTime, SecondsToRead, vChannels);  
+NicPlot(NicSt, data, SecondsToRead);
+
+NicClose();                    
