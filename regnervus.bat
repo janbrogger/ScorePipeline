22f2255b9231345f8154a9bd2ec14737638b5339
janbrogger <jan.brogger@holbergeeg.com>
2016-07-21 11:14:21 +0200

    Fix for nervus DLL problem if present

---
+++ regnervus.bat
@@ -0,0 +1,5 @@
+rem Run as admin
+%windir%\system32\regsvr32.exe "%ProgramFiles%\VIASYS Healthcare\Nicolet EEG\AddinIf.dll"
+%windir%\system32\regsvr32.exe "%ProgramFiles%\VIASYS Healthcare\Nicolet EEG\FileAccess4.dll"
+%windir%\system32\regsvr32.exe "%ProgramFiles%\VIASYS Healthcare\Nicolet EEG\XStorage.dll"
+%windir%\system32\regsvr32.exe "%ProgramFiles%\VIASYS Healthcare\Nicolet EEG\DataStorage.dll"
