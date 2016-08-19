f526614117e49e8f703fde30b8f6ad0d67483df8
janbrogger <jan.brogger@holbergeeg.com>
2016-08-19 19:24:54 +0200

    Iterate over EEG files to find some with the import error

--- matlab/run.m
+++ matlab/run.m
@@ -12,3 +12,32 @@ pop_fileio
 
 disp('Trying import of one EEG file using EEGLAB-fileio');
 pop_fileio('\\hbemta-nevrofil01.knf.local\Rutine\workarea\25001-30000\25001\Patient7t1.e');
+
+
+pop_fileio([scoreBasePath 'janbrogger.e']);
+pop_fileio([scoreBasePath 'Patient15_EEG246_t1.e']);
+
+header = ft_read_header('C:\Users\Jan\Documents\GitHub\ScorePipeline\janbrogger.e');
+
+ft_read_header([scoreBasePath 'janbrogger.e']);
+ft_read_header([scoreBasePath 'Patient15_EEG246_t1.e']);
+
+eegDir = 'C:\LocalDB\Workarea\';
+eegdirlist = dir(eegDir);
+j = 1;
+for i=3:size(eegdirlist,1)        
+     if eegdirlist(i).isdir   
+         eegdirlist2 = dir([eegDir eegdirlist(i).name '\*.e']);  
+         for k=1:size(eegdirlist2,1)    
+             oneeeg = [eegDir eegdirlist(i).name '\' eegdirlist2(k).name];
+             disp(oneeeg);
+             eeglist{j} = oneeeg;
+             j = j + 1;
+         end        
+     end
+end
+
+for i=1:size(eeglist,2)
+    disp(eeglist(i));
+    ft_read_header(eeglist(i));
+end
