0344f1f0f8cbe78e542a36df17f5d84c4a802100
janbrogger <jan.brogger@holbergeeg.com>
2016-11-23 16:55:54 +0100

    Implemented one event details screen

--- matlab/ScoreOneEvent.m
+++ matlab/ScoreOneEvent.m
@@ -1,4 +1,4 @@
-function varargout = ScoreSearchResultDetail(varargin)
+function varargout = ScoreOneEvent(varargin)
 % SCOREPIPELINE MATLAB code for ScorePipeline.fig
 %      SCOREPIPELINE, by itself, creates a new SCOREPIPELINE or raises the existing
 %      singleton*.
@@ -22,13 +22,13 @@ function varargout = ScoreSearchResultDetail(varargin)
 
 % Edit the above text to modify the response to help ScorePipeline
 
-% Last Modified by GUIDE v2.5 23-Nov-2016 15:42:09
+% Last Modified by GUIDE v2.5 23-Nov-2016 16:13:37
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @ScoreSearchResultDetail_OpeningFcn, ...
+                   'gui_OpeningFcn', @ScoreOneEvent_OpeningFcn, ...
                    'gui_OutputFcn',  @ScorePipeline_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
@@ -44,7 +44,7 @@ end
 % End initialization code - DO NOT EDIT
 
 % --- Executes just before ScorePipeline is made visible.
-function ScoreSearchResultDetail_OpeningFcn(hObject, eventdata, handles, varargin)
+function ScoreOneEvent_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -52,14 +52,26 @@ function ScoreSearchResultDetail_OpeningFcn(hObject, eventdata, handles, varargi
 % varargin   command line arguments to ScorePipeline (see VARARGIN)
 
 handles.SearchResultId = -1; 
+handles.SearchResultEventId = -1; 
 if nargin<4 
       handles.SearchResultId = -1;
 else
       handles.SearchResultId = varargin{1};
 end
 
+if nargin<5
+      handles.SearchResultEventId = -1;
+else
+      handles.SearchResultEventId = varargin{2};
+end
+
 if (handles.SearchResultId == -1)
-    error('Cannot show search result details, no argument given');
+    error('Cannot show one event details, no search result ID given');
+end
+
+if not(handles.SearchResultId == -1) && handles.SearchResultEventId == -1    
+    searchResultEventId = ScoreQueryRun(['SELECT MIN(SearchResultEventId) FROM SearchResult_Event WHERE SearchResult_Event.SearchResultId = ' num2str(handles.SearchResultId)]);
+    handles.SearchResultEventId = searchResultEventId{1};
 end
 
 UpdateInfo(handles);
@@ -75,60 +87,11 @@ initialize_gui(hObject, handles, false);
 % uiwait(handles.figure1);
 
 function UpdateInfo(handles)
-comment = ScoreQueryRun(['SELECT SearchResult.Comment FROM [SearchResult] WHERE SearchResultId = ' num2str(handles.SearchResultId)]);
-studyWorkstateNotStarted = ScoreGetWorkstateCount(handles.SearchResultId, 0, 0);
-studyWorkstateInvalid = ScoreGetWorkstateCount(handles.SearchResultId, -1, 0);
-studyWorkstateInProgress = ScoreGetWorkstateCount(handles.SearchResultId, 1, 0);
-studyWorkstateCompleted = ScoreGetWorkstateCount(handles.SearchResultId, 2, 0);
-descriptionWorkstateNotStarted = ScoreGetWorkstateCount(handles.SearchResultId, 0, 1);
-descriptionWorkstateInvalid = ScoreGetWorkstateCount(handles.SearchResultId, -1, 1);
-descriptionWorkstateInProgress = ScoreGetWorkstateCount(handles.SearchResultId, 1, 1);
-descriptionWorkstateCompleted = ScoreGetWorkstateCount(handles.SearchResultId, 2, 1);
-recordingWorkstateNotStarted = ScoreGetWorkstateCount(handles.SearchResultId, 0, 2);
-recordingWorkstateInvalid = ScoreGetWorkstateCount(handles.SearchResultId, -1, 2);
-recordingWorkstateInProgress = ScoreGetWorkstateCount(handles.SearchResultId, 1, 2);
-recordingWorkstateCompleted = ScoreGetWorkstateCount(handles.SearchResultId, 2, 2);
-recordingFileStateNotStarted = ScoreGetFileStateCount(handles.SearchResultId, 0, 2);
-recordingFileStateFound = ScoreGetFileStateCount(handles.SearchResultId, 1, 2);
-recordingFileStateNotFound = ScoreGetFileStateCount(handles.SearchResultId, -1, 2);
-eventcodingWorkstateNotStarted = ScoreGetWorkstateCount(handles.SearchResultId, 0, 3);
-eventcodingWorkstateInvalid = ScoreGetWorkstateCount(handles.SearchResultId, -1, 3);
-eventcodingWorkstateInProgress = ScoreGetWorkstateCount(handles.SearchResultId, 1, 3);
-eventcodingWorkstateCompleted = ScoreGetWorkstateCount(handles.SearchResultId, 2, 3);
-eventWorkstateNotStarted = ScoreGetWorkstateCount(handles.SearchResultId, 0, 4);
-eventWorkstateInvalid = ScoreGetWorkstateCount(handles.SearchResultId, -1, 4);
-eventWorkstateInProgress = ScoreGetWorkstateCount(handles.SearchResultId, 1, 4);
-eventWorkstateCompleted = ScoreGetWorkstateCount(handles.SearchResultId, 2, 4);
-
-
 
 colNames = {'Parameter' 'Value'};
-data = ['Comment' comment;...
-    'Study work state not started' studyWorkstateNotStarted; ...
-    'Study work state invalid' studyWorkstateInvalid; ...
-    'Study work state in progress' studyWorkstateInProgress; ...
-    'Study work state completed' studyWorkstateCompleted; ...    
-    'Description work state not started' descriptionWorkstateNotStarted; ...
-    'Description work state invalid' descriptionWorkstateInvalid; ...
-    'Description work state in progress' descriptionWorkstateInProgress; ...
-    'Description work state completed' descriptionWorkstateCompleted; ...
-    'Recording work state not started' recordingWorkstateNotStarted; ...
-    'Recording work state invalid' recordingWorkstateInvalid; ...
-    'Recording work state in progress' recordingWorkstateInProgress; ...
-    'Recording work state completed' recordingWorkstateCompleted; ...
-    'Recording file state not started' recordingFileStateNotStarted; ...
-    'Recording file state found' recordingFileStateFound; ...    
-    'Recording file state not found' recordingFileStateNotFound; ...    
-    'Eventcoding work state not started' eventcodingWorkstateNotStarted; ...
-    'Eventcoding work state invalid' eventcodingWorkstateInvalid; ...
-    'Eventcoding work state in progress' eventcodingWorkstateInProgress; ...
-    'Eventcoding work state completed' eventcodingWorkstateCompleted; ...
-    'Event work state not started' eventWorkstateNotStarted; ...
-    'Event work state invalid' eventWorkstateInvalid; ...
-    'Event work state in progress' eventWorkstateInProgress; ...
-    'Event work state completed' eventWorkstateCompleted ...
-    ];
-set(handles.searchResultProperties,'data',data,'ColumnName',colNames);
+data = {'SearchResultEventId' handles.SearchResultEventId;};
+
+set(handles.oneEventProperties,'data',data,'ColumnName',colNames);
 
 % --- Outputs from this function are returned to the command line.
 function varargout = ScorePipeline_OutputFcn(hObject, eventdata, handles)
@@ -252,52 +215,34 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 
-% --- Executes on button press in quantifyButton.
-function quantifyButton_Callback(hObject, eventdata, handles)
-% hObject    handle to quantifyButton (see GCBO)
+% --- Executes on button press in nextButton.
+function nextButton_Callback(hObject, eventdata, handles)
+% hObject    handle to nextButton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-ScoreOneEvent(handles.SearchResultId);
+searchResultEventId = ScoreQueryRun(['SELECT MIN(SearchResultEventId) FROM SearchResult_Event ' ...
+    'WHERE SearchResult_Event.SearchResultId = ' num2str(handles.SearchResultId) ...
+    'AND SearchResult_Event.SearchResultEventId > ' num2str(handles.SearchResultEventId) ...
+    ]);
+if not(isnan(searchResultEventId{1}))
+    handles.SearchResultEventId = searchResultEventId{1};
+end
+guidata(hObject, handles);
+UpdateInfo(handles);
 
 
-% --- Executes on button press in deleteButton.
-function deleteButton_Callback(hObject, eventdata, handles)
-% hObject    handle to deleteButton (see GCBO)
+% --- Executes on button press in backButton.
+function backButton_Callback(hObject, eventdata, handles)
+% hObject    handle to backButton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 h = msgbox({'Not implemented'});
 
 
-% --- Executes on button press in updateFiles.
-function updateFiles_Callback(hObject, eventdata, handles)
-% hObject    handle to updateFiles (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
+% --- Executes when selected cell(s) is changed in oneEventProperties.
+function oneEventProperties_CellSelectionCallback(hObject, eventdata, handles)
+% hObject    handle to oneEventProperties (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
+%	Indices: row and column indices of the cell(s) currently selecteds
 % handles    structure with handles and user data (see GUIDATA)
-
-%h = msgbox({'Not implemented yet.'});
-
-t1 = timer();
-set(t1,...
-    'Name','UpdateFilesInfoTimer', ...
-    'TimerFcn', @(~,~) UpdateFilesTimerTick(handles), ...
-    'ExecutionMode','fixedSpacing', ...
-    'Period', 1, ...    
-    'BusyMode', 'drop')
-start(t1);
-%pause(5);
-%Do expensive stuff here
-resetfileStatusQuery = ['UPDATE [dbo].[SearchResult_Recording] SET FileState = 0 WHERE SearchResultId=' num2str(handles.SearchResultId)];  
-ScoreQueryRun(resetfileStatusQuery);  
-resetfileStatusQuery2 = ['UPDATE [dbo].[SearchResult_Recording] SET WorkflowState = 0 WHERE WorkflowState= -1 AND SearchResultId=' num2str(handles.SearchResultId)];  
-ScoreQueryRun(resetfileStatusQuery2);  
-
-ScoreCheckRecordingFiles(handles.SearchResultId);
-stop(t1);
-delete(t1);
-UpdateFilesTimerTick(handles);
-
-
-function UpdateFilesTimerTick(handles)
-UpdateInfo(handles);
-drawnow();
