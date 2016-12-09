5f7a4332b9866744c3b9e4398bc189054e7dab6f
janbrogger <jan.brogger@holbergeeg.com>
2016-12-09 20:15:43 +0100

    Better faked EEG data on development PC

--- ScoreDb/99-FakeRecordingForTest.sql
+++ ScoreDb/99-FakeRecordingForTest.sql
@@ -1,18 +1,26 @@
 USE [HolbergAnon]
 GO
 
-UPDATE [dbo].[Recording]
-   SET [FileName] = N'Patient5_EEG246_t1.e'
-      ,[FilePath] = N'C:\LocalDB\Workarea\45198\'      
+
+declare @FromDate1 datetime       = '2013-09-17 12:27:19'
+declare @ToDate1 datetime         = '2013-09-17 12:51:19'
+declare @FileName1 nvarchar(99)   = N'Patient2_KNF-PORTABEL_t1.e'
+declare @FilePath1 nvarchar(99)   = N'C:\\LocalDB\\Workarea\\45195\\'
+UPDATE [dbo].[Recording] SET [FileName] =  @FileName1,[FilePath] = @FilePath1  WHERE RecordingId % 2 = 0
+UPDATE [dbo].[Event] SET [StartDateTime] = dateadd(day, rand(checksum(newid()))*(1+datediff(day, @FromDate1, @ToDate1)), @FromDate1) WHERE RecordingId % 2 = 0
+UPDATE [dbo].[Event] SET [StartDateTime] = dateadd(second, rand(checksum(newid()))*(1+1199), @FromDate1) WHERE RecordingId % 2 = 0
+
+
+declare @FromDate2 datetime        = '2013-08-29 11:59:43'
+declare @ToDate2 datetime          = '2013-08-29 12:19:19'
+declare @FileName2 nvarchar(99)    = N'Patient9_EEG226_t1.e'
+declare @FilePath2 nvarchar(99)    = N'C:\\LocalDB\\Workarea\\45190\\'
+UPDATE [dbo].[Recording] SET [FileName] =  @FileName2,[FilePath] = @FilePath2  WHERE RecordingId % 2 = 1
+UPDATE [dbo].[Event] SET [StartDateTime] = dateadd(day, rand(checksum(newid()))*(1+datediff(day, @FromDate2, @ToDate2)), @FromDate1) WHERE RecordingId % 2 = 1
+UPDATE [dbo].[Event] SET [StartDateTime] = dateadd(second, rand(checksum(newid()))*(1+1199), @FromDate2) WHERE RecordingId % 2 = 1
 
 
 
-declare @FromDate datetime = '2012-12-07 09:33:39'
-declare @ToDate datetime = '2012-12-07 09:53:39'
-
-
-UPDATE [dbo].[Event] SET [StartDateTime] = dateadd(day, rand(checksum(newid()))*(1+datediff(day, @FromDate, @ToDate)), @FromDate)
-UPDATE [dbo].[Event] SET [StartDateTime] = dateadd(second, rand(checksum(newid()))*(1+1199), @FromDate)
 GO
 
 
