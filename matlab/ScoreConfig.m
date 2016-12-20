ddc2084f37aab3d206fd3cb6fad7f10bd6c9a751
janbrogger <jan.brogger@holbergeeg.com>
2016-12-20 20:42:41 +0100

    Added local Nervus event GUIDs to config

--- matlab/ScoreConfig.m
+++ matlab/ScoreConfig.m
@@ -4,6 +4,22 @@ classdef ScoreConfig
 		databaseServer = 'localhost';
         scoreBasePath = 'C:\Users\Jan\Documents\GitHub\ScorePipeline\';
         eeglabPath = 'C:\Users\Jan\Documents\GitHub\eeglab';
-        debug = 0
+        debug = 1
+        eventGuids = {
+            'EE867C9B-F822-4B1E-AF3E-0B179DD6E0C0' 'Smertestimulerer';
+            'ED986976-8E07-4C44-84CD-7624EB3323FB' 'Graphoelement';
+            'C5C3A3BC-9683-43D6-A703-A74859CEA2A9' 'Event_EEG';
+            'BBD1DA4C-62F4-4EE1-8F28-B5E83B82C575' 'Snakker til pas.';
+            'B0A2B82D-C98F-4699-AF6A-BDE9CF4645A1' 'Øyne lukket';
+            'A5A95601-A7F8-11CF-831A-0800091B5BDA' 'Øyne åpne';
+            'A2A9E9A4-7EED-466B-B7F6-BB48F3140260' 'Event';
+            '93A2CB2C-F420-4672-AA62-18989F768519' 'Detections Inactive';
+            '8DA27322-155A-4A0C-A2D2-6DC03114BAA3' 'Klapper';
+            '71EECE80-EBC4-41c7-BF26-E56911426FB4' 'Recording Paused';
+            '32F2469E-6792-4CAD-8E11-B7747688BB8B' 'Unknown - SCORE?';
+            '056F522F-DDA5-48B9-82E1-1A75C35CBC30' 'Unknown 2 - SCORE?';
+            '01E4E4E6-EA8E-4c08-800C-B841D3C391C1' 'Video Review Progress';
+            'F7744DB4-2EF5-4D31-8259-CBDFA8CDDF73' 'Rykning';
+        }
     end
 end
--- matlab/functions/withEeglabCode/ScoreOpenEegFileInEeglab.m
+++ matlab/functions/withEeglabCode/ScoreOpenEegFileInEeglab.m
@@ -23,15 +23,30 @@ function ScoreOpenEegFileInEeglab(newFilePath, searchResultEventId)
         ekgindex = find(strcmp({EEG.chanlocs(:).labels}, 'EKG'));
         EEG = pop_reref( EEG, [],'exclude',ekgindex);  
         
+        eventsToChangeDuration = find(strcmp({EEG.event(:).value},'Review progress'));
+        EEG.event(eventsToChangeDuration).duration = 0;
+        
+        for i=1:size(EEG.event,2)
+            for j=1:size(ScoreConfig.eventGuids)  
+                if length(EEG.event(i).type) >= 36
+                    if strcmp(EEG.event(i).type(1:36), ScoreConfig.eventGuids(j,1))
+                        EEG.event(i).value = ScoreConfig.eventGuids{j,2};
+                        EEG.event(i).type = ScoreConfig.eventGuids{j,2};
+                    end                
+                end
+            end
+        end
+        
         %Downscale EEG
         EEG.data(ekgindex,:) = EEG.data(ekgindex,:)/5;
         % high-pass filter at 0.5 Hz
         %EEG = pop_eegfilt( EEG, 0.5, 0, [], [0]);
         % low-pass filter at 70 Hz
-        %EEG = pop_eegfilt( EEG, 0, 70, [], [0]);
-        
-        EEG = pop_eegfilt( EEG, 0.5, 70, [], [0], 0, 0, 'fir1', 0);
+        %EEG = pop_eegfilt( EEG, 0, 70, [], [0]);        
+        %EEG = pop_eegfilt( EEG, 0.5, 70, [], [0], 0, 0, 'fir1', 0);
               
+        
+        
         pop_eegplot( EEG, 1, 1, 1);
         clear ekgindex
         EEG.filename = ['searchResultEventId = ' num2str(searchResultEventId)];
