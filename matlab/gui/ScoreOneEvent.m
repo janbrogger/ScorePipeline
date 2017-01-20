644ae645e393c251bd6b86d9391eb1f71c695dcf
janbrogger <jan.brogger@holbergeeg.com>
2017-01-20 14:03:30 +0100

    Started working on vertical scaling

--- matlab/gui/ScoreOneEvent.m
+++ matlab/gui/ScoreOneEvent.m
@@ -358,3 +358,32 @@ guidata(hObject, handles);
     
 
 
+
+
+
+function verticalScaleEdit_Callback(hObject, eventdata, handles)
+% hObject    handle to verticalScaleEdit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of verticalScaleEdit as text
+%        str2double(get(hObject,'String')) returns contents of verticalScaleEdit as a double
+
+str=get(hObject,'String');
+if isempty(str2num(str))
+    set(hObject,'string','0');
+    warndlg('Input must be numerical');
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function verticalScaleEdit_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to verticalScaleEdit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
