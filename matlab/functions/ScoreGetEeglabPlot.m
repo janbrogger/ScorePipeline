2fe9df924770663b3c81f1259848f4eceb1f8bef
janbrogger <jan.brogger@holbergeeg.com>
2017-02-14 11:42:14 +0100

    Vertical physical scaling calculated
    Also, checks if plot window is moved or resized.

--- matlab/functions/ScoreGetEeglabPlot.m
+++ matlab/functions/ScoreGetEeglabPlot.m
@@ -2,7 +2,7 @@ function existingPlot = ScoreGetEeglabPlot()
         
     existingPlot = findobj(0, 'tag', 'EEGPLOT');
     if isempty(existingPlot)
-        error('No EEG plot open in EEGLAB');
+        warning('No EEG plot open in EEGLAB');
     elseif size(existingPlot,1)>1        
         warn('More than one EEGPLOT open, using the first one');        
         existingPlot = existingPlot(1);        
--- matlab/gui/ScoreOneEvent.m
+++ matlab/gui/ScoreOneEvent.m
@@ -78,18 +78,33 @@ handles.timeSpanMinusGaps = -1;
 handles = UpdateInfo(handles);
 
 oldSearchResultRecordingId = handles.SearchResultRecordingId;
-CheckOpenEEG(hObject, handles, oldSearchResultRecordingId);
+%CheckOpenEEG(hObject, handles, oldSearchResultRecordingId);
 
 % Choose default command line output for ScorePipeline
 handles.output = hObject;
 
+handles = StartScaleTimer(handles);
+
+
 % Update handles structure
 guidata(hObject, handles);
-
 initialize_gui(hObject, handles, false);
 
 % UIWAIT makes ScorePipeline wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
+% uiwait(handles.oneEventDetails);
+
+function handles = StartScaleTimer(handles)
+if isfield(handles, 'UpdateScaleInfoTimer')
+    stop(handles.UpdateScaleInfoTimer);
+end    
+handles.UpdateScaleInfoTimer = timer();
+set(handles.UpdateScaleInfoTimer,...
+    'Name','UpdateScaleInfoTimer', ...
+    'TimerFcn', @(x,y)UpdateScaleInfo(), ...
+    'ExecutionMode','fixedSpacing', ...
+    'Period', 1, ...    
+    'BusyMode', 'drop')
+start(handles.UpdateScaleInfoTimer);
 
 function handles = UpdateInfo(handles)
 
@@ -157,92 +172,13 @@ function varargout = ScorePipeline_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-% --- Executes during object creation, after setting all properties.
-function density_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to density (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function density_Callback(hObject, eventdata, handles)
-% hObject    handle to density (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of density as text
-%        str2double(get(hObject,'String')) returns contents of density as a double
-density = str2double(get(hObject, 'String'));
-if isnan(density)
-    set(hObject, 'String', 0);
-    errordlg('Input must be a number','Error');
-end
-
-% Save the new density value
-handles.metricdata.density = density;
-guidata(hObject,handles)
-
-% --- Executes during object creation, after setting all properties.
-function volume_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to volume (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function volume_Callback(hObject, eventdata, handles)
-% hObject    handle to volume (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of volume as text
-%        str2double(get(hObject,'String')) returns contents of volume as a double
-volume = str2double(get(hObject, 'String'));
-if isnan(volume)
-    set(hObject, 'String', 0);
-    errordlg('Input must be a number','Error');
-end
-
-% Save the new volume value
-handles.metricdata.volume = volume;
-guidata(hObject,handles)
-
-% --- Executes on button press in calculate.
-function calculate_Callback(hObject, eventdata, handles)
-% hObject    handle to calculate (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-mass = handles.metricdata.density * handles.metricdata.volume;
-set(handles.mass, 'String', mass);
-
-% --- Executes on button press in reset.
-function reset_Callback(hObject, eventdata, handles)
-% hObject    handle to reset (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-initialize_gui(gcbf, handles, true);
-
 
 % --------------------------------------------------------------------
 function initialize_gui(fig_handle, handles, isreset)
     
 % Update handles structure
 
-guidata(handles.figure1, handles);
+guidata(handles.oneEventDetails, handles);
 
 
 % --- Executes on button press in nextButton.
@@ -308,6 +244,7 @@ set(handles.openButton,'Enable','on');
 
 function openButton_Callback(hObject, eventdata, handles)
 OpenEEG(hObject, handles);
+handles = StartScaleTimer(handles);
 
 
 
@@ -336,10 +273,18 @@ else
 end    
 EnableButtonsAfterWait(handles);
 handles = UpdateInfo(handles);
+
+existingPlot = ScoreGetEeglabPlot();
+if ~isempty(existingPlot)
+    set(existingPlot,'ResizeFcn',@eegPlotResized_CallBack);    
+end
 guidata(hObject, handles);
     
 
-
+function eegPlotResized_CallBack(src,evt)
+thisFigure = findall(0,'Type','figure','tag','oneEventDetails');
+handles = guihandles(thisFigure);
+UpdateScaleInfo(handles);
 
 
 
@@ -351,30 +296,28 @@ function verticalScaleEdit_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of verticalScaleEdit as text
 %        str2double(get(hObject,'String')) returns contents of verticalScaleEdit as a double
 
-str=get(hObject,'String');
+if not(isfield(handles, 'UpdateScaleInfoTimer'))
+    StartScaleTimer(handles);
+end    
+
+stop(handles.UpdateScaleInfoTimer);
+str=get(handles.verticalScaleEdit,'String');
+
 if isempty(str2num(str))
     set(hObject,'string','0');
-    warndlg('Input must be numerical');
-else
-    physicalSizeOfScaleMarkerInCm = str2num(str);
+    warndlg('Input must be numerical');    
+else    
     existingPlot = ScoreGetEeglabPlot();
-    if not(isempty(existingPlot)) && size(existingPlot,1)==1
-        ax1 = findobj('tag','eegaxis','parent',existingPlot);  % axes handle
-        g = get(existingPlot,'UserData');  
-        data = get(ax1, 'userdata');
-        ESpacing = findobj('tag','ESpacing','parent',existingPlot);   % ui handle        
-        g.spacing = str2num(get(ESpacing,'string'));  
-        
-        lines = findobj('type','Line','parent',ax1);
-        %firstLine = lines(1);
-        %position = getpixelposition(handle)
-        
-        currentScale = g.spacing/physicalSizeOfScaleMarkerInCm;
-        newText = strcat(['Vertical EEG scale: ' num2str(currentScale) ' µV/cm']);
-        set(handles.verticalCalculatedEegScale, 'String', newText);
-        guidata(hObject, handles);
-    end    
-end
+    if not(isempty(existingPlot)) && size(existingPlot,1)==1        
+        handles.verticalScaleValidForEegPlotPosition = getpixelposition(existingPlot);    
+    else
+        handles.verticalScaleValidForEegPlotPosition = [];
+    end
+    guidata(hObject, handles);
+end     
+handles = UpdateScaleInfo(handles);
+guidata(hObject, handles);
+start(handles.UpdateScaleInfoTimer);
 
 
 
@@ -389,3 +332,70 @@ function verticalScaleEdit_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+function handles = UpdateScaleInfo(handles)
+
+if not(exist('handles' ,'var')) || isempty(handles)
+    thisFigure = findall(0,'Type','figure','tag','oneEventDetails');
+    if not(isempty(thisFigure))    
+        handles = guidata(thisFigure);
+    end    
+end
+
+%if handles is empty, no figure found, means that figure has closed
+if not(isempty(handles)) 
+    str=get(handles.verticalScaleEdit,'String');
+    if isempty(str2num(str))
+        set(handles.verticalScaleEdit,'string','0');
+        warndlg('Input must be numerical');
+    else
+        physicalSizeOfScaleMarkerInCm = str2num(str);
+        existingPlot = ScoreGetEeglabPlot();
+        if not(isempty(existingPlot)) && size(existingPlot,1)==1
+            currentEegPlotPosition = getpixelposition(existingPlot);
+            if isfield(handles,'verticalScaleValidForEegPlotPosition') ...
+                && not(isempty(handles.verticalScaleValidForEegPlotPosition)) ...
+                && all(currentEegPlotPosition == handles.verticalScaleValidForEegPlotPosition)
+
+                ax1 = findobj('tag','eegaxis','parent',existingPlot);  % axes handle
+                g = get(existingPlot,'UserData');  
+                ESpacing = findobj('tag','ESpacing','parent',existingPlot);   % ui handle        
+                g.spacing = str2num(get(ESpacing,'string'));  
+
+                %Get data about the whole plot
+                verticalSizeOfPlotInMicroVolts = ax1.YLim(2)-ax1.YLim(2);
+                axisPosition = getpixelposition(ax1);
+                verticalSizeOfPlotInPixels = axisPosition(4);        
+                %sprintf('%g', verticalSizeOfPlotInPixels)
+
+                %Get data about the SCORE scale eye
+                scoreEyeAxis = findobj('tag','eyeaxes','parent',existingPlot); 
+                scoreEyeAxisVerticalScaleInMicrovolts = g.spacing*4;
+                scoreEyeAxisPosition = getpixelposition(scoreEyeAxis);
+                scoreEyeAxisVerticalScaleInPixels = scoreEyeAxisPosition(4);
+                scoreEyeAxisVerticalScaleInMicroVoltsPerCentimeter = scoreEyeAxisVerticalScaleInMicrovolts/physicalSizeOfScaleMarkerInCm;
+
+                newText = strcat(['Vertical EEG scale: ' num2str(scoreEyeAxisVerticalScaleInMicroVoltsPerCentimeter) ' µV/cm']);
+                set(handles.verticalCalculatedEegScale, 'String', newText);            
+            else            
+                newText = strcat(['Vertical EEG scale: unknown (plot moved or resized)']);
+                set(handles.verticalCalculatedEegScale, 'String', newText);
+            end
+        else
+            newText = strcat(['Vertical EEG scale: unknown (no plot, or >1 plot)']);
+            set(handles.verticalCalculatedEegScale, 'String', newText);
+            stop(handles.UpdateScaleInfoTimer);
+        end    
+    end    
+end
+
+% --- Executes during object deletion, before destroying properties.
+function oneEventDetails_DeleteFcn(hObject, eventdata, handles)
+% hObject    handle to oneEventDetails (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+if isfield(handles, 'UpdateScaleInfoTimer')
+    stop(handles.UpdateScaleInfoTimer);
+end    
