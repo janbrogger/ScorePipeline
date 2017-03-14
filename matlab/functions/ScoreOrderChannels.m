379cf704e2edcd122813083500291566682b57b9
janbrogger <jan.brogger@holbergeeg.com>
2017-03-14 16:46:01 +0100

    Working on ordering channels

--- matlab/functions/ScoreOrderChannels.m
+++ matlab/functions/ScoreOrderChannels.m
@@ -1,18 +1,20 @@
 function EEG = ScoreOrderChannels(EEG, montageOrder)
 
     for i=1:size(montageOrder, 2)
-        index = find(cellfun(@(x) strcmp(x, montageOrder{i}), {EEG.chanlocs.labels}));
-        if ~isempty(index)
-            row1 = EEG.data(index,:);
+        disp(['Channel list at step ' num2str(i) ' is: ' EEG.chanlocs.labels]);
+        index1 = find(cellfun(@(x) strcmp(x, montageOrder{i}), {EEG.chanlocs.labels}));                
+        if ~isempty(index1) && index1 ~= i             
+            row1 = EEG.data(index1,:);
             row2 = EEG.data(i,:);
-            chaninfo1 = EEG.chaninfo(index,:);
-            chaninfo2 = EEG.chaninfo(i,:);
-            
-            EEG.data(i) = row1;
-            EEG.data(index) = row2;
-            
-            EEG.chaninfo(index) = chaninfo2;
-            EEG.chaninfo(i) = chaninfo1;
+            chaninfo1 = EEG.chanlocs(index1,:);
+            chaninfo2 = EEG.chanlocs(i,:);
+            disp(['Swapping channels ' EEG.chanlocs(index1).labels ' and ' EEG.chanlocs(i).labels]);
+
+            EEG.data(index1,:) = row2;
+            EEG.data(i,:) = row1;            
+
+            EEG.chanlocs(index1) = chaninfo2;
+            EEG.chanlocs(i) = chaninfo1;            
             
         end
     end
