cdcf2c7febcae19e7f812605d9072dbb4ecc1805
janbrogger <jan.brogger@holbergeeg.com>
2017-05-16 15:25:45 +0200

    Dynamic pivoting of annotations into events is working (but some values are null)

--- ScoreDb/202-Pivot-2.sql
+++ ScoreDb/202-Pivot-2.sql
@@ -4,32 +4,48 @@ USE HolbergAnon
 IF OBJECT_ID('tempdb..##PivotResult2') IS NOT NULL DROP TABLE ##PivotResult2
 IF OBJECT_ID('tempdb..#AnnotationUsers') IS NOT NULL DROP TABLE #AnnotationUsers
 IF OBJECT_ID('tempdb..#AnnotationFields') IS NOT NULL DROP TABLE #AnnotationFields
+IF OBJECT_ID('tempdb..#PivotingElements') IS NOT NULL DROP TABLE #PivotingElements
 IF OBJECT_ID('tempdb..#AnnotationsAndUsers') IS NOT NULL DROP TABLE #AnnotationsAndUsers
 GO
-
-
-
-
+--SELECT * FROM ##PivotResult
 
 --Now pivot a second time 
 --Store users who did annotations
 SELECT * INTO #AnnotationUsers FROM [User] 	WHERE [User].UserName='jcbr' OR [User].UserName='eivaan'
-SELECT * FROM #AnnotationUsers
+--SELECT * FROM #AnnotationUsers
 --Store fieldnames for annotations
 SELECT DISTINCT FieldName INTO #AnnotationFields FROM SearchResult_AnnotationConfig
-SELECT * FROM #AnnotationFields
+--SELECT * FROM #AnnotationFields
 --Now make columns names for each annotation that includes userid
+DECLARE @UserIds AS VARCHAR(MAX)
+SELECT @UserIds = COALESCE(@UserIds + ',[' + CAST(UserId as varchar) + ']',  '[' + cast(UserId as varchar)+ ']')   FROM #AnnotationUsers
+PRINT @UserIds
+
+--Generate the pivoting parts
+SELECT FieldName,        
+	   ROW_NUMBER() OVER(ORDER BY (SELECT 0)) AS RowNumber,	   
+	   N' PIVOT(MIN('+FieldName+') FOR UserId'+CONVERT(nvarchar(10),ROW_NUMBER() OVER(ORDER BY (SELECT 0)))+' IN ('+REPLACE(@UserIds,'[','[F'+FieldName+'_')+
+	   N' ) AS piv'+CONVERT(nvarchar(10),ROW_NUMBER() OVER(ORDER BY (SELECT 0))) AS PivotingElement
+    INTO #PivotingElements
+	FROM #AnnotationFields
+	ORDER BY FieldName
+SELECT * FROM #PivotingElements
+
 SELECT FieldName, 
        #AnnotationUsers.UserId, 
 	   CONCAT(FieldName,'_',#AnnotationUsers.UserId ) AS ColumnName, 
 	   ROW_NUMBER() OVER(ORDER BY (SELECT 0)) AS RowNumber,
-	   CONCAT('U', ROW_NUMBER() OVER(ORDER BY (SELECT 0))) AS UserIdMultiplexed
+	   N' CONCAT(''F'+FieldName+'_'',UserId) AS UserId'+CONVERT(nvarchar(10),ROW_NUMBER() OVER(ORDER BY (SELECT 0))) AS UserIdMultiplexed,	   
+	   N'MAX([F'+FieldName+'_'+CONVERT(nvarchar(10),UserId)+']) AS '+CONCAT(FieldName,'_',#AnnotationUsers.UserId ) AS PivotSelectPart	   
 	INTO #AnnotationsAndUsers 
 	FROM #AnnotationFields
 	CROSS JOIN #AnnotationUsers 
 	ORDER BY FieldName
 SELECT * FROM #AnnotationsAndUsers
-	
+
+DECLARE @UserIdMultiplexed AS VARCHAR(MAX)
+SELECT @UserIdMultiplexed = COALESCE(@UserIdMultiplexed + ',' + UserIdMultiplexed + '',  '' + UserIdMultiplexed+ '') FROM #AnnotationsAndUsers
+PRINT @UserIdMultiplexed	
 
 DECLARE @AnnotationList1 AS VARCHAR(MAX)
 SELECT @AnnotationList1 = COALESCE(@AnnotationList1 + ',' + FieldName + '',  '' + FieldName+ '')   FROM (SELECT DISTINCT FieldName FROM SearchResult_AnnotationConfig) AS x
@@ -39,44 +55,29 @@ DECLARE @AnnotationUserCount AS int
 SELECT @AnnotationUserCount = COUNT(*) FROM #AnnotationsAndUsers
 PRINT @AnnotationUserCount
 
---Generate a pivoted columns list
-DECLARE @cnt INT = 0;
-WHILE @cnt < @AnnotationUserCount
-BEGIN
-   --SELECT 'MAX('
-   --PRINT 'PIVOT(MIN('
-   SET @cnt = @cnt + 1;
-END;;
-
 DECLARE @UserIds1 AS VARCHAR(MAX)
 SELECT @UserIds1 = COALESCE(@UserIds1 + ',[P' + CAST(UserId as varchar) + ']',  '[P' + cast(UserId as varchar)+ ']')   FROM #AnnotationUsers
 
 DECLARE @UserIds2 AS VARCHAR(MAX)
 SELECT @UserIds2 = COALESCE(@UserIds2 + ',[U' + CAST(UserId as varchar) + ']',  '[U' + cast(UserId as varchar)+ ']')   FROM #AnnotationUsers
 
+DECLARE @InitialMaxPart AS VARCHAR(MAX)
+SELECT @InitialMaxPart = COALESCE(@InitialMaxPart + ',' + PivotSelectPart, PivotSelectPart )   FROM #AnnotationsAndUsers
+PRINT @InitialMaxPart
 
+DECLARE @PivotPart AS VARCHAR(MAX)
+SELECT @PivotPart = COALESCE(@PivotPart + PivotingElement, PivotingElement)   FROM #PivotingElements
+PRINT @PivotPart
 
 DECLARE @PivotSql2 AS VARCHAR(MAX)
 SET @PivotSql2 = 
 	N'SELECT  '+	
-	N'EventId, '+
-	N'MAX([P2]) AS SpikeStartSample_2, ' +
-	N'MAX([P25]) AS SpikeStartSample_25, ' +
-	N'MAX([U2]) AS SpikeEndSample_2, ' +
-	N'MAX([U25]) AS SpikeEndSample_25 ' +
+	N'EventId, '+@InitialMaxPart+
 	N' INTO ##PivotResult2 ' +
-	N' FROM (SELECT EventId, CONCAT(''P'',UserId) AS UserId1, CONCAT(''U'',UserId) AS UserId2, '+@AnnotationList1+' FROM ##PivotResult) AS src '+
-	N' PIVOT('+
-    N'  MIN(SpikeStartSample)'+
-	N'  FOR UserId1'+
-    N'  IN ('+ @UserIds1 +')'+
-	N') AS piv1 ' +
-	N' PIVOT('+
-    N'  MIN(SpikeEndSample)'+
-	N'  FOR UserId2'+
-    N'  IN ('+ @UserIds2 +')'+
-	N') AS piv2 ' +
+	N' FROM (SELECT EventId, '+@UserIdMultiplexed+','+@AnnotationList1+' FROM ##PivotResult) AS src '+
+	@PivotPart +
 	N' GROUP BY EventId '
 PRINT @PivotSql2
 EXEC(@PivotSql2)
 SELECT * FROM ##PivotResult2
+
